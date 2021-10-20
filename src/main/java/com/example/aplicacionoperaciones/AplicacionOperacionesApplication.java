9708904b17efb457b6765a78905afa307ae8bafc
adamY97 <adam24891@gmail.com>
2021-10-20 15:02:19 +0200

    Initial commit

---
+++ src/main/java/com/example/aplicacionoperaciones/AplicacionOperacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionoperaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionOperacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionOperacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aplicacionoperaciones/AplicacionOperacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionoperaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionOperacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
